389e595d737fdb0013ee39e9f679c34d096323c8
haze <skdml20022@gmail.com>
2024-10-29 19:02:04 +0900

    chore: 프로젝트 세팅

---
+++ src/test/java/com/server/epigram/EpigramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.epigram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpigramApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
